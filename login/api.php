2c3df1b120f9dd94a2378adc007da70b81b2bd33
subaharahak <subaharahak@gmail.com>
2025-08-15 23:30:02 +0530

    Update api.php

--- login/api.php
+++ login/api.php
@@ -727,10 +727,11 @@ else {
 curl_close($ch);
 ob_flush();
 flush();
-echo "<b>1REQ Result:</b> $result1<br><br>";
-echo "<b>2REQ Result:</b> $result2<br><br>";
+//echo "<b>1REQ Result:</b> $result1<br><br>";
+//echo "<b>2REQ Result:</b> $result2<br><br>";
 
 ?>
+
 
 
 
